7b0f75863e546e1f61f81688edae01099f8cca23
azaremde <azaremde@gmail.com>
2020-05-19 18:32:18 +0200

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -9,7 +9,7 @@ class DBAccessor {
   }
 
   public static init(): void {
-    const uri = `mongodb+srv://${Environment.mongodbUser}:${Environment.mongodbPassword}@social-network-u1s7h.mongodb.net/test?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${Environment.mongodbUser}:${Environment.mongodbPassword}@cluster0-oxkrf.mongodb.net/test?retryWrites=true&w=majority`;
 
     DBAccessor.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 100 });
     DBAccessor.client.connect();
